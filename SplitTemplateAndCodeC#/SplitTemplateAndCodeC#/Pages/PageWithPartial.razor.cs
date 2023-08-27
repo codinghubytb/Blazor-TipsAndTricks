d3d824eb6a52c3334324a6c06b5a1ec8c755f806
CodingHub <95412136+codinghubytb@users.noreply.github.com>
2023-08-27 12:34:07 +0200

    First Commit

---
+++ SplitTemplateAndCodeC#/SplitTemplateAndCodeC#/Pages/PageWithPartial.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace SplitTemplateAndCodeC_.Pages
+{
+    public partial class PageWithPartial
+    {
+        string Title { get; set; } = "Title : Page With Partial File";
+        string Description { get; set; } = "Description : Page With Partial File";
+    }
+}
---
+++ SplitTemplateAndCodeC#/SplitTemplateAndCodeC#/ViewModel/PageWithBaseClassViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SplitTemplateAndCodeC_.ViewModel
+{
+    public class PageWithBaseClassViewModel : ComponentBase
+    {
+        protected string Title { get; set; } = "Title : Page With Base Class File";
+        protected string Description { get; set; } = "Description : Page With Base Class File";
+    }
+}
